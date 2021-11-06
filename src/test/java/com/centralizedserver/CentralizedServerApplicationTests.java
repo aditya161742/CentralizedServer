36469407e24abc792207d8fdfdcac17a2444a145
aditya ojha <adityaojha91@yahoo.com>
2021-11-06 07:30:17 -0400

    Centralized Server first commit

---
+++ src/test/java/com/centralizedserver/CentralizedServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.centralizedserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralizedServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
